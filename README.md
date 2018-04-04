e014c69f1a4acfa59f0b5caaee2f2fe063834941
Meghan Debity <mdebity@gmail.com>
2018-04-04 09:31:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Challenge 1: Create a function called 'go' that takes 2 arguments: direction (forwards, backwards, etc.) and speed (mph). The function, when invoked, will print out the following in your console (for example): "The car is moving forward, at 25 mph."
+
+
+
+Challenge 2: Instead of the function printing to the console, print to the DOM 
---
+++ main.js
@@ -0,0 +1,14 @@
+const go = (direction, mph) => {
+    console.log('The car is moving ${direction}, at ${mph} mph');
+}
+go("forward", 100)
+
+//alternative answer to challenge
+//write function
+// function go(direction, speed){
+//     console.log('The car is moving ${direction} at ${speed} mph')
+// }
+// //invoke action; call the function
+// go("forward",34)
+// go("backward", 2)
+// go("forward", 100)
